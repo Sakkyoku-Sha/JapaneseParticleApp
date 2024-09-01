0175298f36320973a292a63b24a5a13ddb3d5bc9
Ryan Anderson <RyanAnderson0147@gmail.com>
2024-09-01 18:21:41 +0900

    Add Basic Attributions to Header

--- jpa/src/components/HeaderComponent.vue
+++ jpa/src/components/HeaderComponent.vue
@@ -36,10 +36,31 @@ const toggleInfoBox = () => {
             </div>
           
         </div>
-        <div v-if="showInfoBox" class="info-box">
-            <p>Attributions for the app:</p>
-            <!-- Add your attributions here -->
+        <div v-if="showInfoBox" class="info-box-overlay">
+            <div v-if="showInfoBox" class="info-box">
+                <div class="arrow-up"></div>
+                <div class="section">
+                    <h2>Website Developed by:</h2> 
+                    <h3>Ryan Anderson</h3>
+                </div>
+                <hr>
+                <div class="section">
+                    <p><strong class="align-left">LinkedIn:</strong><a href="https://www.linkedin.com/in/ryan-anderson24/" target="_blank">linkedin.com/in/ryan-anderson24</a></p>
+                    <p><strong class="align-left">Email:</strong><a href="mailto:RyanAnderson0147@gmail.com">RyanAnderson0147@gmail.com</a></p>
+                    <p><strong class="align-left">Source Code:</strong><a href="https://github.com/Sakkyoku-Sha/JapaneseParticleApp" target="_blank">Available here</a></p>
+                </div>
+                <hr>
+                <div class="section">
+                    <h2>Technology and Resources:</h2>
+                    <p><strong class="align-left">Grammar Explanations:</strong>Powered by OpenAPI</p>
+                    <p><strong class="align-left">Japanese Parsing:</strong>Enabled by <a href="https://github.com/atilika/kuromoji" target="_blank">Kuromoji</a></p>
+                    <p><strong class="align-left">Japanese Input:</strong>Enabled by <a href="https://wanakana.com" target="_blank">WanaKana.js</a></p>
+                    <p><strong class="align-left">UI Framework:</strong><a href="https://vuejs.org/" target="_blank">vuejs</a></p>
+                </div>    
+                <hr>                
+            </div>
         </div>
+       
     </header>
 </template>
 
@@ -64,7 +85,14 @@ const toggleInfoBox = () => {
 }
 .header-right-group {
     display: flex;
-    column-gap: 15px;
+    align-items: center;
+}
+.header-right-group i {
+    margin-right: 10px;
+    color: #fff;
+}
+select {
+    margin-right: 10px;
 }
 .info-button {
     background-color: #fff;
@@ -75,11 +103,55 @@ const toggleInfoBox = () => {
     height: 30px;
     cursor: pointer;
 }
+.info-box-overlay {
+    position: absolute;
+    top: 55px; /* Adjust as needed */
+    right: 5px; /* Adjust as needed */
+    width: 500px; /* Adjust as needed */
+    z-index: 1000;
+}
 .info-box {
-    background-color: #444;
+    position: relative;
+    background-color: #646161;
     color: #fff;
     padding: 10px;
-    margin-top: 10px;
     border-radius: 5px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.9);
+}
+.arrow-up {
+    position: absolute;
+    top: -10px; /* Adjust as needed */
+    right: 10px; /* Algn with the ? button */
+    width: 0;
+    height: 0;
+    border-left: 10px solid transparent;
+    border-right: 10px solid transparent;
+    border-bottom: 10px solid #646161;
+    box-shadow: 0 -10px 10px -10px rgba(0, 0, 0, 0.9); /* Shadow on the upper edge */
+
+}
+
+h2 {
+    font-size: 1.5em;
+    margin-bottom: 10px;
+}
+p {
+    margin: 10px 0;
+}
+a {
+    color: #1a73e8;
+    text-decoration: none;
+}
+a:hover {
+    text-decoration: underline;
+}
+.align-left {
+    display: inline-block;
+    width: 220px; /* Adjust as needed */
+    text-align: left;
+}
+.align-left + a {
+    display: inline-block;
+    text-align: right;
 }
 </style>
